695c93db97c861c7d1a95d26d0976552c3e86af6
Loay Halawani <loayhalawani0@gmail.com>
2025-01-08 21:12:12 +0200

    edit

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,34 +1,34 @@
 public class BinarySearch {
 	
-	static int binarySearchIter(int[] arr, int key, int low, int high) {
-		while(low <= high) {
-			int mid = (int) Math.floor((low + high) / 2);
+	static int binarySearchIter(int[] arr, int key, int left, int right) {
+		while(left <= right) {
+			int mid = (int) Math.floor((left + right) / 2);
 			if(key == arr[mid]) {
 				return mid + 1;
 			}
 			else if(key < arr[mid]) {
-				high = mid - 1;
+				right = mid - 1;
 			}
 			else {
-				low = mid + 1;
+				left = mid + 1;
 			}
 		}
-		return 0;
+		return -1;
 	}
 
-	static int binarySearchRec(int[] arr, int key, int low, int high) {
-		if(low > high) {
-			return 0;
+	static int binarySearchRec(int[] arr, int key, int left, int right) {
+		if(left > right) {
+			return -1;
 		}
-		int mid = (int) Math.floor((low + high) / 2);
+		int mid = (int) Math.floor((left + right) / 2);
 		if(key == arr[mid]) {
 			return mid + 1;
 		}
 		else if(key < arr[mid]) {
-			return binarySearchRec(arr, key, low, mid - 1);
+			return binarySearchRec(arr, key, left, mid - 1);
 		}
 		else {
-			return binarySearchRec(arr, key, mid + 1, high);
+			return binarySearchRec(arr, key, mid + 1, right);
 		}
 	}
 
